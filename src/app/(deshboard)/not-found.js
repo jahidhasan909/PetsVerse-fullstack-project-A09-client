bf3aef3bead511061ed7e9a13142a3de9ed3888b
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-05-20 22:45:46 +0600

    added 404 not found page

---
+++ src/app/(deshboard)/not-found.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
+import './globals.css'
+
+export default function NotFound() {
+    return (
+        <section className="min-h-screen flex items-center justify-center p-6 ">
+
+  
+            <div className="flex flex-col items-center justify-center gap-6 w-full max-w-5xl">
+
+            
+                <div className="flex items-center p-4 justify-center w-full max-h-[75vh] ">
+                    <Image
+                        src="https://i.ibb.co.com/YBcSfkNF/Chat-GPT-Image-May-20-2026-at-10-11-19-PM-1.png"
+                        alt="404"
+                        width={1000}
+                        height={800}
+                        priority
+                        className="w-full object-cover rounded-2xl"
+                    />
+                </div>
+
+                
+                <Link
+                    href="/"
+                    className="px-8 py-3 mt-4 rounded-full bg-black text-white hover:scale-105 transition-transform duration-300 shadow-md"
+                >
+                    Go Home
+                </Link>
+
+            </div>
+
+        </section>
+    );
+}
